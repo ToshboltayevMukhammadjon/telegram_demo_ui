abb97f915d221c77b031c7f224b3366710b7308f
Mukhammadjon <toshboltayevmuxammadjon@gmail.com>
2021-08-03 21:57:06 +0500

    firebase added and chat page

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,63 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:telegram_demo/ui/login/login_page.dart';
 import 'package:telegram_demo/ui/pages/main/main_page.dart';
+import 'package:telegram_demo/ui/pages/main/unread/unread_page.dart';
 
-void main() {
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MainPage(),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
+class _MyAppState extends State<MyApp> {
+  String uid = '';
+
+  statusColor(Color color) async {
+    try {
+      await FlutterStatusbarcolor.setStatusBarColor(color, animate: true);
+      await FlutterStatusbarcolor.setNavigationBarWhiteForeground(false);
+    } on PlatformException catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    statusColor(Colors.white);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        brightness: Brightness.light,
+      ),
+      home: FutureBuilder<bool>(
+        future: checkIfUserLoggedIn(),
+        builder: (context, snapshot) => (snapshot.hasData && snapshot.data!)
+            ? MainPage()
+            : LoginPage(),
+      ),
+    );
+  }
+
+  Future<bool> checkIfUserLoggedIn() async {
+    SharedPreferences shared = await SharedPreferences.getInstance();
+    bool userLoggedIn = (shared.getString("id") ?? "").isNotEmpty;
+    if (userLoggedIn) setState(() => uid = shared.getString("id") ?? '');
+    return userLoggedIn;
+  }
+}
--- lib/ui/pages/main/unread/unread_page.dart
+++ lib/ui/pages/main/unread/unread_page.dart
@@ -1,11 +1,101 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-class UnreadPage extends StatelessWidget {
-  const UnreadPage({Key? key}) : super(key: key);
+import 'package:flutter/services.dart';
+import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:telegram_demo/main.dart';
+import 'package:telegram_demo/ui/pages/chat/chat2_page.dart';
+
+class UnreadPage extends StatefulWidget {
+  UnreadPage({Key? key, required this.uid}) : super(key: key);
+
+  final String uid;
+
+  @override
+  _UnreadPageState createState() => _UnreadPageState();
+}
+
+class _UnreadPageState extends State<UnreadPage> {
+  GoogleSignIn googleSignIn = GoogleSignIn();
+  String? userId;
+
+  @override
+  void initState() {
+    super.initState();
+    userId = widget.uid;
+    statusColor(Colors.white);
+  }
+
+  statusColor(Color color) async {
+    try {
+      await FlutterStatusbarcolor.setStatusBarColor(color, animate: true);
+      FlutterStatusbarcolor.setNavigationBarWhiteForeground(false);
+    } on PlatformException catch (e) {
+      debugPrint(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Unread"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        brightness: Brightness.light,
+        actions: <Widget>[
+          RaisedButton(
+            child: Text("out"),
+            onPressed: () async {
+              await googleSignIn.signOut();
+              SharedPreferences sharedPreferences =
+              await SharedPreferences.getInstance();
+              sharedPreferences.clear();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => MyApp()),
+              );
+            },
+          )
+        ],
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection("users").snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data != null) {
+            return ListView.builder(
+              itemCount: ((snapshot.data!).docs).length,
+              itemBuilder: (context, index) =>
+                  buildItem(snapshot.data!.docs[index]),
+            );
+          }
+          return Container(
+            alignment: Alignment.center,
+            child: Text("Malumot yo'q"),
+          );
+        },
+      ),
     );
+  }
+
+  buildItem(doc) {
+    return (userId != doc["id"])
+        ? InkWell(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Chat2Page(docs: doc)),
+      ),
+      child: Card(
+        color: Colors.lightBlue,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            child: Center(
+              child: Text(doc["name"]),
+            ),
+          ),
+        ),
+      ),
+    )
+        : Container();
   }
 }
